c3dc373236e767190140b52ae8f9b4e5c3b7a857
Černý Jan <j.cerny@quadient.com>
2021-04-23 18:02:35 +0200

    the 1st public beta of v2.0

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # YouTrack Wider
 This add-on injects JavaScript into YouTrack web pages for wider view. 
 
-Compatible with YouTrack *2018.1 Build 39916* and lower.
+Compatible with YouTrack *2021.1.11513* and newer.
 
 Before add-on: 
-![before](Before.png)
-![before](Before2.png)
-![before](Before3.png)
+![before](Before-Lite.png)
 
 After add-on:
-![after](After.png)
-![after](After2.png)
-![after](After3.png)
+![after](After-Lite.png)
 
 ## Release Notes
+### v2.0
+* stopped support older version than 2021.1
+* reduced package size 
+
 ### v1.6
 * fixed bug: Does not work in "Use experimental version of full page issue view by default" 
 
@@ -26,12 +26,15 @@ After add-on:
 * size reduction
 
 ## Add-on for FireFox
-[YouTrack Wider v1.6](https://addons.mozilla.org/cs/firefox/addon/youtrack-wider/)
+[YouTrack Wider](https://addons.mozilla.org/cs/firefox/addon/youtrack-wider/)
 
 ## Add-on for Chrome
-* download file [YouTrack Wider v1.6](https://github.com/cernyjan/YouTrack-Wider/blob/master/youtrack_wider-1.6-an+fx.xpi)
+* download file [YouTrack Wider](https://github.com/cernyjan/YouTrack-Wider/releases)
 * extract it on the file system
 * open Extensions page in browser
 * click the “Developer mode” checkbox to activate it
 * click the “Load unpacked extension” button
 * Navigate to the extension’s directory and open it
+
+## The latest (unsupported) v1.0
+[YouTrack Wider v1.6](https://github.com/cernyjan/YouTrack-Wider/blob/master/youtrack_wider-1.6-an+fx.xpi)
--- YouTrack-Wider.js
+++ YouTrack-Wider.js
@@ -1,84 +1,76 @@
-$( document ).ready(function() {
-	console.log("YouTrack Wider is Online");
+window.addEventListener("load", function() {
+    console.log("YouTrack Wider is Online");
+	
+	var mode = '';
+	var timeout = 100;
+	var loop = 0;
 
-	function oldTheme() {
-		$('.ring-header-react.ring-header-react_issues.ng-scope').css({ 'max-width': '100%' });
-		$('#id_l\\.I\\.bigPanel').css({ 'max-width': '100%' });
-		$('#id_l\\.I\\.mainPanel').css({ 'max-width': '100%' });
+	var timer = setInterval(ytTimer, 100);
 
-		window.addEventListener('popstate', function(e){
-			var currenthash = window.location.hash;
-			if (currenthash.indexOf("#newissue") >= 0 || currenthash.indexOf("&newissue") >= 0) 
-			{
-				$('#id_l\\.I\\.ni\\.newIssuePane').css({ 'max-width': '100%' });
-			}
-		})
-
-		var urlParams = new URLSearchParams(window.location.search);
-		if (urlParams.has('draftId'))
-		{
-			$('.container_width.rootNewIssue').css({ 'max-width': '100%' });
-			$('#id_l\\.N\\.ni\\.newIssuePane').css({
-		   	'padding-left': '32px',
-		   	'padding-right': '32px',
-		   	'max-width': '100%'
-			});
-			$('.footer-placeholder.footer-placeholder_right').css({ 'padding-left': '32px' });
-		} 
-
-		$( "#id_l\\.I\\.sb\\.filters_div" ).css( "width", "298px" );
-		$('.vert-list-wrp').css({ 'width': '255px' });
-
-		$('.center.issues-center').css({ 'margin-left': '316px' });
-		$('.default-layout.container.container_width.container_padding').css({ 'max-width': '100%' });
-
-		function iteration() {
-			myVar = setInterval(checkDialogForSavedSearch, 1000);
+	function GetOldElement() {
+		var ytIssueViewElements = document.getElementsByTagName("yt-issue-view");
+		if (ytIssueViewElements.length) {
+			return ytIssueViewElements[0];
 		}
-
-		function checkDialogForSavedSearch() {
-			if ($('div#id_l\\.I\\.sb\\.s\\.ewfd\\.editWatchFolderDlg').length > 0)
-			{
-				if ($('div#id_l\\.I\\.sb\\.s\\.ewfd\\.editWatchFolderDlg[style*="width: 100%;"]').length == 0)
-				{
-					$('#id_l\\.I\\.sb\\.s\\.ewfd\\.searchField').css({ 'width': '100%' });
-					$('#id_l\\.I\\.sb\\.s\\.ewfd\\.watchFolderName').css({ 'width': '100%' });
-				}			
-			}		    
+		else {
+			return null;
 		}
-
-		iteration();
 	}
 
-	var app = "";
-	var loop = 1;
-	var skip = 50
-
-	var appExists = setInterval(function() {
-		console.log(loop);
-		if (loop > skip)
-		{
-			clearInterval(appExists);
-			oldTheme();
+	function GetLiteElement() {
+		var divTags = document.getElementsByTagName("div");
+		for (var i = 0; i < divTags.length; i++) {
+			if (divTags[i].hasAttribute('data-test')) {
+				if (divTags[i].getAttribute('data-test').indexOf('issue-container') > -1) {
+					return divTags[i];
+				}
+			}
 		}
-		else
-		{
-			app = $('.app__container');
-	 		if ($(app).length) {
-	    		clearInterval(appExists);
-	    		$(app).css({ 'max-width': '100%' });
-				$( "div[data-test='wikiText']" ).css({ 'max-width': '100%' });
-	 		}
-	 		else
-	 		{
-		 		if ( $( "div[baseuri='/']" ).length )
-		 		{
-		 			clearInterval(appExists);
-					oldTheme();
-		 		}
-		 	}
+		return null; 
+	}
 
-	 		loop = loop + 1;
-	 	}
-	}, 100);
+	function MakeWider() {
+		if (mode.indexOf('Lite') >= 0 ) {
+			GetLiteElement().style.width = '100%';
+		}	
+		else if (mode.indexOf('Old') >= 0) {
+			GetOldElement().style.maxWidth = '100%';
+		}
+		else {
+			console.log("Unexpected mode: '", mode, "'");
+		}
+		clearInterval(timer);
+	}
+
+	function IsWiderable() {
+		var oldElement = GetOldElement();
+		var liteElement = GetLiteElement();
+		if (oldElement != null || liteElement != null) {
+			if (oldElement != null) {
+				mode = 'Old';
+			}
+			else {
+				mode = 'Lite';
+			}
+			return true;
+		}
+		else {
+			return false;
+		}  		
+	}
+
+	function ytTimer() {
+		if (loop < timeout)
+		{
+			if(IsWiderable()) {
+				MakeWider();
+			}
+		}
+		else {
+			clearInterval(timer);
+			console.log("Cannot make wider");
+		}
+
+		loop = loop + 1;
+	}	
 });
